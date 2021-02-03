1ec8c2f365069e1908641abebd7127e4db92e873
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-02-03 08:58:24 +0100

    Created a base of running player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10.0f;
+
+	private void Update()
+	{
+		Vector3 position = transform.position;
+
+		position.x += moveSpeed * Time.deltaTime;
+
+		transform.position = position;
+	}
+}
